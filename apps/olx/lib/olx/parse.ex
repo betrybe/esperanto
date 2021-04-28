0e183003ed59edf9dc279d3c50b8285e3d89f211
Guilherme Castro <guilherme.torres.castro@gmail.com>
2021-04-28 10:14:24 -0300

    WIP documenting

--- apps/olx/lib/olx/parse.ex
+++ apps/olx/lib/olx/parse.ex
@@ -4,6 +4,13 @@ defmodule Olx.Parser do
   @type ast_node() :: NaryTree.Node.t()
   @type tree() :: NaryTree.t()
 
+  @doc """
+  Parse given input
+
+    * `real_time_url`- Url to fetch bus coordinates, default to `https://temporeal.pbh.gov.br/?param=C"`
+    * `pull_interval`- Time in miliseconds that should be pulled", default to `60s`
+    * `bus_line_provider`- Default to `Crawler.CachexBusLineProvider`
+  """
   @callback parse(Walker.t(), tree(), ast_node(), keyword())::{ ast_node(), Walker.t()}
   @callback should_parse(Walker.t(), tree(), node(), keyword())::boolean()
 
