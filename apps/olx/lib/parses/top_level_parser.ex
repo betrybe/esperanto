bb76206614da1f5a128a744e35f8fae0caa5e2b2
Guilherme Castro <guilherme.torres.castro@gmail.com>
2021-04-28 09:43:47 -0300

    coments

--- apps/olx/lib/parses/top_level_parser.ex
+++ apps/olx/lib/parses/top_level_parser.ex
@@ -14,43 +14,46 @@ defmodule Olx.Parsers.TopLevel do
   end
 
   # No more input finished the parser
-  def astify(%Olx.Walker{input: "", rest: ""}, tree, _node , _parsers, _selected_parser) do
+  defp astify(%Olx.Walker{input: "", rest: ""}, tree, _node , _parsers, _selected_parser) do
     tree
   end
 
   # No more input finish using the fallback parser
-  def astify(%Olx.Walker{rest: ""} = input, tree, node, parsers, :find_parse) do
+  defp astify(%Olx.Walker{rest: ""} = input, tree, node, parsers, :find_parse) do
     astify(input, tree, node, parsers, {Olx.Parsers.PlainText, []})
   end
 
   # Parser found, execute-it
-  def astify(input, tree, node, parsers, {parser, opts}) do
+  defp astify(input, tree, node, parsers, {parser, opts}) do
     {node, walker} = parser.parse(input, tree, node, opts)
     tree = NaryTree.add_child(tree, node)
     astify(walker, tree, node, parsers, nil)
   end
 
   # Find parser
-  def astify(walker, tree, node, parsers, :find_parse) do
+  defp astify(walker, tree, node, parsers, :find_parse) do
     selected_parsers = select_parse(walker, tree, node, parsers)
     astify(walker, tree, node, parsers, selected_parsers)
   end
 
   # no parser found,  walk
-  def astify(walker, tree, node, parsers, :walk) do
+  defp astify(walker, tree, node, parsers, :walk) do
     walker = Walker.walk(walker)
     astify(walker, tree, node, parsers, :find_parse)
   end
 
-  def select_parse(_input, [parse]) do
+  # Only one parse found, ready to go
+  defp select_parse(_input, [parse]) do
     parse
   end
 
-  def select_parse(_input, []) do
+  # no parser found,  walk
+  defp select_parse(_input, []) do
     :walk
   end
 
-  def select_parse(walker, tree, node, parsers) do
+  # find parsers that should be executed
+  defp select_parse(walker, tree, node, parsers) do
     filtered_parsers = Enum.filter(parsers, fn {parser, opts} ->
       apply(parser, :should_parse, [walker, tree, node, parsers, opts])
     end)
