c10b9007cb83518b062d7a0715b74c16f7fc6bd0
Guilherme Castro <guilherme.torres.castro@gmail.com>
2021-05-12 16:55:37 -0300

    fix: dialyzer

--- apps/esperanto/lib/trybe/esperanto/parsers/top_level_parser.ex
+++ apps/esperanto/lib/trybe/esperanto/parsers/top_level_parser.ex
@@ -70,7 +70,6 @@ defmodule Esperanto.Parsers.TopLevel do
 
   # no parser found,  walk
   defp astify(walker, tree, parent_id, opts, :walk) do
-    IO.puts()
     walker = Walker.walk(walker)
     astify(walker, tree, parent_id, opts, :find_parse)
   end
